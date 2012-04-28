23b3d89cf1c9fd8ac6d571ba64f8bc08fd092529
jeffhandley <jeff@jeffhandley.com>
2012-04-28 01:41:10 -0700

    Rewrite with input from Matt Hamilton (@mabster), using Budgie.

--- GrammarBots/TwitterBot/Program.cs
+++ GrammarBots/TwitterBot/Program.cs
@@ -3,61 +3,119 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace TwitterBot
 {
-    class Program
+    public class Program
     {
+        public class Mistake
+        {
+            public string TwitterSearch { get; set; }
+            public string RegexPattern { get; set; }
+            public string RegexReplace { get; set; }
+            public string Explanation { get; set; }
+        }
+
+        public static Mistake HaveCame = new Mistake
+        {
+            TwitterSearch = @"""have came""",
+            RegexPattern = @"(.*\b)have came(\b.*)",
+            RegexReplace = @"$1**have come**$2",
+            Explanation = @"It's **have come**, not **have came**, thanks! See http://www.english-test.net/forum/ftopic23316.html"
+        };
+
+        public static Mistake ALot = new Mistake
+        {
+            TwitterSearch = @"""alot""",
+            RegexPattern = @"(.*\b)alot(\b.*)",
+            RegexReplace = null,
+            Explanation = @"It's **a lot**, not **alot**. ""Alot"" isn't a word. Thanks!"
+        };
+
+        public static Mistake OfInsteadOfHave = new Mistake
+        {
+            TwitterSearch = @"""should of"" OR ""would of"" OR ""could of"" OR ""might of"" OR ""must of""",
+            RegexPattern = @"(?:^|\s+)(((c|sh|w)ould\s+)of)(?:(?:\s+[^c])|\.|$)",
+            RegexReplace = null,
+            Explanation = @"Should have, would have, could have, might have, must have. Have, not of. Thanks!"
+        };
+
+        public static Mistake Definately = new Mistake
+        {
+            TwitterSearch = "definately",
+            RegexPattern = @"(?:^|\s+)(definately)(?:(?:\s+)|\.|$)",
+            RegexReplace = null,
+            Explanation = @"It's not ""definately"", it's ""definitely"", with an ""i"" and no ""a"", thanks! See http://www.d-e-f-i-n-i-t-e-l-y.com."
+        };
+
+        static Mistake[] Mistakes = { HaveCame, ALot, OfInsteadOfHave, Definately };
+
         static void Main(string[] args)
         {
-            TweetSharp.TwitterService service = new TweetSharp.TwitterService();
-
             string consumerKey = Environment.GetEnvironmentVariable("Grammar_FTFY_consumerKey");
             string consumerSecret = Environment.GetEnvironmentVariable("Grammar_FTFY_consumerSecret");
-            string token = Environment.GetEnvironmentVariable("Grammar_FTFY_accessToken");
-            string tokenSecret = Environment.GetEnvironmentVariable("Grammar_FTFY_accessTokenSecret");
+            string accessToken = Environment.GetEnvironmentVariable("Grammar_FTFY_accessToken");
+            string accessTokenSecret = Environment.GetEnvironmentVariable("Grammar_FTFY_accessTokenSecret");
 
-            service.AuthenticateWith(consumerKey, consumerSecret, token, tokenSecret);
+            var service = new Budgie.TwitterClient(consumerKey, consumerSecret);
+            service.Authenticate(accessToken, accessTokenSecret);
 
-            long lastTweet = service.ListTweetsOnUserTimeline().FirstOrDefault().Id;
-
-            while (true)
+            service.GetUserTimelineAsync().ContinueWith(t =>
             {
-                var haveCame = service.SearchSince(lastTweet + 1, "\" have came \"");
+                var lastTweet = t.Result.Result.First().Id;
 
-                if (haveCame.Statuses.Any())
+                while (true)
                 {
-                    lastTweet = haveCame.Statuses.First().Id;
-                }
-
-                foreach (var haveCameMistake in haveCame.Statuses)
-                {
-                    var correction = Regex.Replace(haveCameMistake.Text, " have came ", " have **COME** ", RegexOptions.IgnoreCase);
-
-                    if (correction != haveCameMistake.Text)
+                    foreach (var mistake in Mistakes)
                     {
-                        var mtPrefix = "MT @" + haveCameMistake.Author.ScreenName + " ";
+                        // Need to avoid a closure error where mistake gets modified before the async callback continues
+                        var mistakeLocal = mistake;
 
-                        string reply;
-                        if (mtPrefix.Length + correction.Length > 140)
+                        service.SearchAsync(mistakeLocal.TwitterSearch, 1, lastTweet + 1).ContinueWith(s =>
                         {
-                            reply = "@" + haveCameMistake.Author.ScreenName + " It's 'have **COME**', not 'have CAME.'";
-                        }
-                        else
-                        {
-                            reply = mtPrefix + correction;
-                        }
+                            if (s.Result.Result.Any())
+                            {
+                                var tweet = s.Result.Result.First();
+                                lastTweet = tweet.Id;
 
-                        var tweet = service.SendTweet(reply, haveCameMistake.Id);
+                                var reply = "@" + tweet.User.ScreenName + " " + mistakeLocal.Explanation;
 
-                        Console.WriteLine(haveCameMistake.CreatedDate.ToShortTimeString());
-                        Console.WriteLine("@" + haveCameMistake.Author.ScreenName + " " + haveCameMistake.Text);
-                        Console.WriteLine("    " + reply);
-                        Console.WriteLine(tweet.Id);
-                        Console.WriteLine();
+                                if (mistakeLocal.RegexReplace != null)
+                                {
+                                    var correction = Regex.Replace(tweet.Text, mistakeLocal.RegexPattern, mistakeLocal.RegexReplace, RegexOptions.IgnoreCase);
+
+                                    if (correction != tweet.Text)
+                                    {
+                                        {
+                                            var mtPrefix = "MT @" + tweet.User.ScreenName + " ";
+
+                                            if (mtPrefix.Length + correction.Length <= 140)
+                                            {
+                                                reply = mtPrefix + correction;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                service.ReplyToAsync(tweet.Id, reply).ContinueWith(r =>
+                                {
+                                    Console.WriteLine(tweet.CreatedAt.ToShortTimeString());
+                                    Console.WriteLine("@" + tweet.User.ScreenName + " " + tweet.Text);
+                                    Console.WriteLine("    " + reply);
+                                    Console.WriteLine(tweet.Id);
+                                    Console.WriteLine();
+
+                                    // Look beyond our own tweet
+                                    lastTweet = r.Result.Result.Id;
+                                });
+                            }
+                        });
+
+                        System.Threading.Thread.Sleep(TimeSpan.FromMinutes(30));
                     }
                 }
-            }
+            }).Wait();
         }
     }
 }
